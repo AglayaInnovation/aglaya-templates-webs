eece3125f97f3c738dc2a953b3a44e4bc2ea5923
WillCarv <willcarv007@hotmail.com>
2024-08-05 00:29:22 -0600

    Cambio de estructura y se agregaron los hover

--- tarjeta-virtual-acm/tailwind.config.js
+++ tarjeta-virtual-acm/tailwind.config.js
@@ -5,25 +5,14 @@ module.exports = {
   ],
   theme: {
       extend: {
-        maxWidth:{
-          '390': '390px',
-        },
-        maxHeight: {
-          '844': '844px',
-        },
-        minWidth: {
-          '360': '360px',
-        },
-        minHeight: {
-          '640': '640px',
-        },
         colors: {
           "Blue-very-Dark": "#1C5081",
           "Blue-dark": "#2978A4",
           "Blue-dark-medium": "#007EA9",
         },
         fontFamily: {
-          poppins: ['Poppins', 'sans-serif'],
+          poppins:['poppins_700', 'sans-serif'],
+          poppins:['poppins_500', 'sans-serrif'],
       },
     },
   },

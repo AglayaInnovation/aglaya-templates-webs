9f87f64ff2be371a308700368d15b3415124ee53
Amanda Campos <75186589+amandaxo21@users.noreply.github.com>
2024-09-21 11:26:18 -0600

    Feature/145 navbar monster codes (#5)
    * cambios realizados
    
    * Mensaje sobre los cambios en .DS_Store
    
    * Incluyendo cambios en .DS_Store
    
    * chore: reestructure folder
    
    * chore: Delete all fonts and implement fonts online
    
    - Delete all fonts
    - Add new rule git ignore monster_codes
    
    * chore: Delete file .DS_Store
    
    ---------
    
    Co-authored-by: Jose A. Gonzalez <jglez.dev@gmail.com>

--- monzter-codes/tailwind.config.js
+++ monzter-codes/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '300px', // Nuevo breakpoint para pantallas de 300px o más
+      },
+      fontSize: {
+        'xxs': ['0.425rem', { lineHeight: '0.575rem' }], // 10px font-size, 14px line-height
+      },
+      backdropSaturate: {
+        '180': '180%',
+      },
+      colors: {
+        'custom-blue': '#00B2FF',
+      },
+      fontFamily: {
+        Inter:['Inter'],
+        Cousine:['Cousine']
+      },
+      backgroundImage: {
+        'custom-pattern': "url('../../assets/images/mc_textura.svg')",
+      },
+    },
   },
   plugins: [],
 }
